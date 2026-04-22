8812cb42de03e46bc89449d88beb307aa43c8c8b
Dikran H. <dhachikian@expert-bg.org>
2026-04-22 08:36:08 +0000

    Python Course L01

---
+++ ex02_zan01.py
@@ -0,0 +1,27 @@
+# %%
+print('Hello Python')
+# %%
+print( 5 + 6)
+
+# %%
+a = 5.0
+b = 6
+
+print( a + b )
+
+# !!! Крайно лоша идея !!!
+# a = 'Hello Python'
+# %%
+
+print( type(a) )
+
+# %%
+
+print( id(a) )
+
+# %%
+a = 1000
+
+print( id(a) )
+
+# %%
---
+++ ex04_zan01.py
@@ -0,0 +1,50 @@
+# %%
+
+s1 = 'Lorem Ipsum is simply dummy'
+s2 = "Lorem Ipsum is simply dummy"
+
+# %%
+s3 = '''Lorem Ipsum is 
+simply dummy text of.
+
+The printing and 
+typesetting'''
+
+print(s1)
+print(s2)
+print(s3)
+# %%
+######################################
+# Function foo print "Hello Python"
+# Author: Me
+# Created: ...
+# ###################################
+def foo():
+    '''Function foo prints Hello Python
+    created: today
+    author: Me'''
+    print('Hello Python')
+
+# %%
+foo()
+# %%
+print(foo.__doc__)
+
+# %%
+
+print( s1[4] )
+
+# %%
+print( s1[0:5])
+# %%
+print( s1[0:20:2])
+
+# %%
+
+print( 'python'.upper() )
+
+# %%
+
+print('-' * 30 )
+
+# %%
